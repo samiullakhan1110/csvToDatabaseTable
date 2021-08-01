b98b92430e4ea7d40bbfad243b6401a92e7d4dc8
samiullakhan1110 <88276549+samiullakhan1110@users.noreply.github.com>
2021-08-01 18:34:23 +0530

    First commit

---
+++ hello.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const mysql = require("mysql");
+const fastcsv = require("fast-csv");
+
+let stream = fs.createReadStream("Sales Records.csv");
+let csvData = [];
+let csvStream = fastcsv
+  .parse()
+  .on("data", function(data) {
+    csvData.push(data);
+  })
+  .on("end", function() {
+    // remove the first line: header
+    csvData.shift();
+
+    // create a new connection to the database
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: " ",
+      database: "demo"
+    });
+
+    // open the connection
+    connection.connect(error => {
+      if (error) {
+        console.error(error);
+      } else {
+        let query =
+          "INSERT INTO category (id, name, description, created_at) VALUES ?";
+        connection.query(query, [csvData], (error, response) => {
+          console.log(error || response);
+        });
+      }
+    });
+  });
+
+stream.pipe(csvStream);
+
